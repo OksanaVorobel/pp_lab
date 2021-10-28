c82ae84f8897fccc80bca7c775edb55c6ce21cf3
Solomiia Kreminets <50262209+Qwr03@users.noreply.github.com>
2021-10-28 16:34:58 +0300

    Add files via upload

--- lab6/alembic/env.py
+++ lab6/alembic/env.py
@@ -2,11 +2,17 @@ from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
-
+from sqlalchemy import create_engine
 from alembic import context
-from database.database import Base
+
+import os
+import sys
+sys.path.append(os.getcwd())
+
+from models import Base
 
 
+target_metadata = [Base.metadata]
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -25,7 +31,7 @@ target_metadata = Base.metadata
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
-
+engine = create_engine("mysql+mysqlconnector://root:newpassword0@localhost/lab6", echo=True)
 
 def run_migrations_offline():
     """Run migrations in 'offline' mode.
@@ -63,7 +69,7 @@ def run_migrations_online():
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
-
+    # target_metadata.create_all(engine)
     with connectable.connect() as connection:
         context.configure(
             connection=connection, target_metadata=target_metadata
